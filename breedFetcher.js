7687f725120dd3bac2ffe0131584356dab06754c
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2021-11-04 20:25:20 -0400

    Code refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,22 @@
 const request = require('request');
 
-const type = process.argv.slice(2)[0];
+const fetchBreedDescription = function(breedName, callback) {
+  
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, function(error, _response, body) {
+    
+    if (error) {
+      return callback(error, null);
+    } else { // No error
+      const data = JSON.parse(body);
+      // No breed found
+      if (!data.length) {
+        return callback("Breed not found.", null);
+      }
+      //Breed found and no error
+      return callback(null, data[0].description);
+    }
+    
+  });
+};
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + type, function(error, _response, body) {
-  
-  if (error) {
-    return console.log(error);
-  }
-  const data = JSON.parse(body);
-  if (!data.length) {
-    return console.log("Breed not found.");
-  }
-  //Breed found and no error
-  console.log(data[0].description);
-  
-});
+module.exports = { fetchBreedDescription };
